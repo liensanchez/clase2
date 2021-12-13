ac523558934be796dbb016640e8df70b0d4073ea
Lien! <80282938+liensanchez@users.noreply.github.com>
2021-12-13 19:48:37 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let nombre=prompt ("ingrese nombre");
+if (nombre==""){
+  alert ("NO NAME");
+}
+else{
+  alert ("Nombre ingresado "+nombre)
+}
+let color=prompt('Promo "El juego del Calamar" elegi un color Rojo o Azul"');
+if (color=="Rojo"){
+  alert("Descuento en tu hidrogel a $1000 en efectivo");
+}
+else if (color=="rojo"){
+  alert("Descuento en tu funda a $1000 en efectivo");
+}
+else if (color=="Azul"){
+  alert("Descuento en tu Cable a $1000 en efectivo");
+}
+else if (color=="azul"){
+  alert("Descuento de 10% en efectivo");
+}
